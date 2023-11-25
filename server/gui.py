33fc2d7739d3f16672f2a36035e98e5e9e4a87e3
Sebastien Gilbert <sebastiengilbert73@yahoo.ca>
2023-11-25 17:15:41 -0500

    .\server\gui.py: Loads configuration parameters from file

--- server/gui.py
+++ server/gui.py
@@ -1,19 +1,56 @@
 import tkinter as tk
 import logging
+import types
 from types import SimpleNamespace
 import sys
 sys.path.append("../src/wakeword_service")
 from listener import WakewordListener
+import xml.etree.ElementTree as ET
 
 logging.basicConfig(level=logging.DEBUG, format='%(asctime)-15s %(levelname)s %(message)s')
+
+def load_config(filepath="./wakeword_service_gui_config.xml"):
+    config = SimpleNamespace()
+    tree = ET.parse(filepath)
+    root_elm = tree.getroot()
+    for root_child_elm in root_elm:
+        if root_child_elm.tag == 'command_on_wakeword':
+            config.command_on_wakeword = root_child_elm.text
+        elif root_child_elm.tag == 'chunk_size':
+            config.chunk_size = int(root_child_elm.text)
+        elif root_child_elm.tag == 'model_wakeword':
+            config.model_wakeword = root_child_elm.text
+        elif root_child_elm.tag == 'inference_framework':
+            config.inference_framework = root_child_elm.text
+        elif root_child_elm.tag == 'channels':
+            config.channels = int(root_child_elm.text)
+        elif root_child_elm.tag == 'rate':
+            config.rate = int(root_child_elm.text)
+        elif root_child_elm.tag == 'sleep_after_detection_in_seconds':
+            config.sleep_after_detection_in_seconds = float(root_child_elm.text)
+        else:
+            raise NotImplementedError(f"gui.load_config(): Not implemented element <{root_child_elm.tag}>")
+    return config
+
+config = load_config("./wakeword_service_gui_config.xml")
 
 global_params = SimpleNamespace()
 global_params.is_running = False
 global_params.start_stop_btn = None
 global_params.indicator_light = None
-global_params.wakerword_listener = WakewordListener()
+global_params.wakerword_listener = WakewordListener(
+    command_on_wakeword=config.command_on_wakeword,
+    chunk_size=config.chunk_size,
+    model_path=config.model_wakeword,
+    inference_framework=config.inference_framework,
+    channels=config.channels,
+    rate=config.rate,
+    sleep_after_detection_in_seconds=config.sleep_after_detection_in_seconds
+)
 global_params.wakerword_listener.daemon = True
 global_params.wakerword_listener.start()
+
+
 
 def start_stop():
     global global_params
